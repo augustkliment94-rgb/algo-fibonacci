11e0649a1986f6dbf50fcad4c94340e73030cb3f
August Kliment <augustkliment94@gmail.com>
2025-11-10 20:07:03 -0600

    August, Thomas, Julie

--- fibonacci.js
+++ fibonacci.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
-  return 0;
+  if (num === 0) return 0;
+  if (num === 1) return 1;
+  let a = 0;
+  let b = 1;
+  
+  for (let i = 2; i <= num; i++){
+    c = a + b;
+    a = b;
+    b = c;
+  }
+  return c;
 }
 
 module.exports = fibonacci;
